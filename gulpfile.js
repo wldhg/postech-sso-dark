139f4ad598b20ea5c83f6b842555d52cdd0e20b3
Jio <jio@wldh.org>
2020-03-03 00:15:45 +0900

    1.0.5: selection limited & responsibility improved

--- gulpfile.js
+++ gulpfile.js
@@ -8,8 +8,8 @@ const gAutoPrefixer = require('gulp-autoprefixer');
 
 const updateJSON = require('update-json-file');
 
-// Get Package Information
-const version = '1.0.4';
+// Set Package Information
+const version = '1.0.5';
 
 // Build style
 const buildSass = () => gulp.src([
